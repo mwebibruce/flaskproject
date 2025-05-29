b37959d3a975e15d3976d9357029fbfa58a1da4c
mwebibruce <brucemwebi@outlook.com>
2025-05-29 13:02:14 +0300

    installed mysqllconnect

--- testflask.py
+++ testflask.py
@@ -254,3 +254,33 @@ def index():
 @app.route("/nursing/specialties")
 def specialties():
     return ("pediatrics")
+@app.route('/hello/<user>')
+def hello_name(user):
+   return render_template('index.html', name=user)
+import mysql.connector
+#INSERT INTO `staff` (`id`, `fullnames`, `dateofbirth`, `specialty`) VALUES (NULL, 'Joyce Wairimu', '2025-05-09', 'othopidic ');
+#INSERT INTO `staff` (`id`, `fullnames`, `dateofbirth`, `specialty`) VALUES (NULL, 'Sandra gesare', '2025-05-14', 'oncology nursing
+@app.route("/nursing/nurses")
+def nurses():
+  mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="",
+  database="nursesheduledb")
+  print(mydb)
+  mycursor = mydb.cursor()
+
+#mycursor.execute("SHOW DATABASES")
+
+#for x in mycursor:
+ # print(x)
+
+
+  
+  mycursor.execute("SELECT * FROM staff WHERE fullnames LIKE 'a%';")
+
+  myresult = mycursor.fetchall()
+
+  for x in myresult:
+    print(x)
+  return (x)
